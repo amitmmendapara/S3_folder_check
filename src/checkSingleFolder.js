d399d69bb75bc7a8cf5bd73638d54a0fa608ff1e
Amit Mendapara <amitpaneli@gmail.com>
2026-08-27 15:42:46 +0530

    add 3 console

--- src/checkSingleFolder.js
+++ src/checkSingleFolder.js
@@ -8,6 +8,9 @@ async function main() {
   if (!folderId) {
     console.error("Usage: node src/checkSingleFolder.js <folderId>");
     process.exit(1);
+    console.error("Usage: node src/checkSingleFolder.js <folderId>");
+    console.error("Usage: node src/checkSingleFolder.js <folderId>");
+    console.error("Usage: node src/checkSingleFolder.js <folderId>");
   }
 
   try {
